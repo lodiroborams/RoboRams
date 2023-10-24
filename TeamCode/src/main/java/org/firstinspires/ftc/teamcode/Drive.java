8f3b9612c3fdcfb1224f328d2e6eccd87b81131c
RoboRams <roborams13615@gmail.com>
2023-10-23 23:10:56 -0400

    basic Drive train

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
@@ -1,24 +1,81 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name = "Drive")
-public class Drive extends OpMode {
 
-    DcMotor motor;
+@TeleOp
+public class Drive extends LinearOpMode {
+    private DcMotor FrontleftMotor;
+    private DcMotor FrontrightMotor;
+    private DcMotor BackleftMotor;
+    private DcMotor BackrightMotor;
 
-    @Override
-    public void init() {
 
-        telemetry.addData("Initialization:", "is a success");
-                telemetry.update();
+    //private Servo positioning;
 
+    private Servo lift1;
+    private Servo lift2;
+
+
+
+    public void runOpMode() {
+        FrontleftMotor = hardwareMap.get(DcMotor.class, "front_left");
+        FrontrightMotor = hardwareMap.get(DcMotor.class, "front_right");
+        BackleftMotor = hardwareMap.get(DcMotor.class, "back_left");
+        BackrightMotor = hardwareMap.get(DcMotor.class, "back_right");
+        lift1 = hardwareMap.get(Servo.class, "left_servo");
+        lift2 = hardwareMap.get(Servo.class, "right_servo");
+
+
+        FrontleftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        FrontrightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        BackleftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        BackrightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+
+        waitForStart();
+        if (opModeIsActive()) {
+        }
+
+
+        double y = -gamepad1.left_stick_x;     // TODO ITERATION 1 gamepad 1
+        double x = gamepad1.left_stick_y; // counteract improer strafing
+        double rx = gamepad1.right_stick_x;
+
+
+
+        double rotX = x * Math.cos(0) - y * Math.sin(0);
+        double rotY = x * Math.sin(0) + y * Math.cos(0);
+
+
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        double frontleftPower = (rotY + rotX - rx) / denominator;
+        double backleftPower = (rotY - rotX + rx) / denominator;
+        double frontrightPower = (rotY - rotX - rx) / denominator;
+        double backrightPower = (rotY + rotX + rx) / denominator;
+
+        FrontleftMotor.setPower(frontleftPower);
+        FrontrightMotor.setPower(frontrightPower);
+        BackleftMotor.setPower(backleftPower);
+        BackrightMotor.setPower(backrightPower);
+
+        if (gamepad1.dpad_down){;
+
+            lift1.setPosition(0);
+            lift2.setPosition(0);
+
+
+        }
+
+        if (gamepad1.dpad_up){;
+
+            lift1.setPosition(1);
+            lift2.setPosition(1);
+
+        }
     }
 
-    @Override
-    public void loop() {
-
-    }
 }
