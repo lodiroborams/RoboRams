dba5c29b0f9e947728dfda38c25ca1fe2ec05941
RoboRams <roborams13615@gmail.com>
2023-11-25 21:34:54 -0500

    anything

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
 
@@ -12,12 +13,12 @@ public class Drive extends LinearOpMode {
     private DcMotor FrontrightMotor;
     private DcMotor BackleftMotor;
     private DcMotor BackrightMotor;
+    private DcMotor ArmMotor;
 
-
-    //private Servo positioning;
-
-    private Servo lift1;
-    private Servo lift2;
+    //Servo
+    private Servo Claw;
+    private Servo Launcher;
+    private Servo Wrist;
 
 
     public void runOpMode() {
@@ -25,59 +26,79 @@ public class Drive extends LinearOpMode {
         FrontrightMotor = hardwareMap.get(DcMotor.class, "front_right");
         BackleftMotor = hardwareMap.get(DcMotor.class, "back_left");
         BackrightMotor = hardwareMap.get(DcMotor.class, "back_right");
-        lift1 = hardwareMap.get(Servo.class, "left_servo");
-        lift2 = hardwareMap.get(Servo.class, "right_servo");
+        ArmMotor = hardwareMap.get(DcMotor.class, "arm_motor");
+        Claw = hardwareMap.get(Servo.class, "claw");
+        Wrist = hardwareMap.get(Servo.class, "wrist");
+        Launcher = hardwareMap.get(Servo.class, "launcher");
 
 
         FrontleftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         FrontrightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         BackleftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         BackrightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        ArmMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
 
         waitForStart();
         if (opModeIsActive()) {
-            
-            double lift1.setPosition(0);
-            double lift2.setPosition(0);
-            
+
+            ArmMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+            Claw.setPosition(0);
+
+            Wrist.setPosition(0);
+
+            Launcher.setPosition(0);
+
             while (opModeIsActive()) {
+/*
+  see what the inputs are for this code right now right down what you think
+  left stick should be rotating controls
+  right stick should be forward, left, back, right
+*/
+                double strafe_stick = -gamepad1.left_stick_y;     // TODO ITERATION 1 gamepad 1
+                double rotate_stick = -gamepad1.left_stick_x; // counteract improer strafing
+                double forwardback_stick = -gamepad1.right_stick_y;
+
+                double updown_stick = -gamepad2.left_stick_y;     // TODO ITERATION 1
+                double wrist_updown = -gamepad2.right_stick_y;
+
+                double frontleftPower = forwardback_stick + rotate_stick - strafe_stick;
+                double frontrightPower = forwardback_stick - rotate_stick + strafe_stick;
+                double backleftPower = forwardback_stick + rotate_stick + strafe_stick;
+                double backrightPower = forwardback_stick -rotate_stick - strafe_stick;
+
+                double updown_stickPower = updown_stick;
+                double wrist_updownPower = wrist_updown;
 
 
-                double y = -gamepad1.left_stick_x;     // TODO ITERATION 1 gamepad 1
-                double x = gamepad1.left_stick_y; // counteract improer strafing
-                double rx = gamepad1.right_stick_x;
-
-
-                double rotX = x * Math.cos(0) - y * Math.sin(0);
-                double rotY = x * Math.sin(0) + y * Math.cos(0);
-
-
-                double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-                double frontleftPower = (rotY + rotX - rx) / denominator;
-                double backleftPower = (rotY - rotX + rx) / denominator;
-                double frontrightPower = (rotY - rotX - rx) / denominator;
-                double backrightPower = (rotY + rotX + rx) / denominator;
-
-                FrontleftMotor.setPower(frontleftPower);
-                FrontrightMotor.setPower(frontrightPower);
+                FrontleftMotor.setPower(-frontleftPower);
+                FrontrightMotor.setPower(-frontrightPower);
                 BackleftMotor.setPower(backleftPower);
                 BackrightMotor.setPower(backrightPower);
 
+                ArmMotor.setPower(updown_stickPower);
 
-                if (gamepad1.a) {
-                    ;
+                Wrist.setPosition(updown_stickPower);
 
-                    lift1.setPosition(.4);
-                    lift2.setPosition(.4);
+
+
+
+                if (gamepad2.right_bumper) {
+                    Claw.setPosition(0);
+
                 }
 
-                if (gamepad1.x) {
-                    ;
 
-                    lift1.setPosition(.6);
-                    lift2.setPosition(.6);
+                if (gamepad2.left_bumper) {
+                    Claw.setPosition(.1);
+
                 }
+
+                if (gamepad2.x){
+                    Launcher.setPosition(1);
+                }
+
             }
         }
     }
