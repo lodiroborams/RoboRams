394b39218592c69fb1e34e11fd2e742b02c7ae9d
Justin13615 <145405577+Justin13615@users.noreply.github.com>
2023-11-26 01:22:50 -0500

    Update Drive.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
@@ -84,6 +84,8 @@ public class Drive extends LinearOpMode {
 
                 ArmMotor.setPower(updown_stickPower);
 
+                Wrist.setPosition(wrist_updownPower);
+
 
 
                 if (gamepad2.right_bumper) {
